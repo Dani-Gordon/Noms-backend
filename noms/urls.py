1abcb2c2d13d7998ec94a6b8f0d72d67c3cc582e
Dani Gordon <lilyluvr723@gmail.com>
2022-04-14 17:44:43 +0200

    added serializer

--- noms/urls.py
+++ noms/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from recipes.views import ListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('recipes/', ListView.as_view())
 ]
